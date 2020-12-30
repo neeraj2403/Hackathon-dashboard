40b22a78a3c18a784a2935c91c93ba77eb8151a8
athlkrshna <athulkrishna1999@gmail.com>
2020-12-31 01:52:50 +0530

    health workers and school

--- project.py
+++ project.py
@@ -14,5 +14,17 @@ def data():
         data = json.load(f)
     return data
 
+@app.route("/health_workers")
+def health_workers():
+    with open('./Datasets/healthworkers_index') as h:
+        health_workers = json.load(h)
+    return health_workers
+
+@app.route("/school")
+def school():
+    with open('./Datasets/school_index') as s:
+        school = json.load(s)
+    return school
+
 if __name__=='__main__':
     app.run(debug=True)
