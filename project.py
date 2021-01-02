11ce85f585e2845caf3d9c85ed018aed6f69c74a
vaishnav-197 <vaishnavea18@gmail.com>
2021-01-02 12:18:09 +0530

    1

--- project.py
+++ project.py
@@ -1,8 +1,47 @@
-from flask import Flask, render_template
+from flask import Flask, render_template , request
 import requests
 import json
 
 app= Flask(__name__)
+
+with open('./Datasets/active_vulnerability_index') as f:
+        active_vulnerability_index= json.load(f)
+
+
+
+with open('./Datasets/healthworkers_index') as h:
+        health_workers = json.load(h)
+
+
+
+with open('./Datasets/school_index') as s:
+        school = json.load(s)
+
+@app.route('/phase', methods=['post', 'get'])
+def login():
+    message = ''
+    if request.method == 'POST':
+        name = request.form.get('name')  # access the data inside 
+        age = request.form.get('age')
+        state = request.form.get('state')
+        occupation = request.form.get('occupation')
+        person = request.form.get('person with comorbidities ')
+        comorbidities = request.form.get('comorbidities')
+
+    
+        
+
+    return render_template('index.html')
+
+
+
+
+
+
+
+
+
+
 
 @app.route("/")
 def index():
@@ -10,20 +49,17 @@ def index():
 
 @app.route("/data")
 def data():
-    with open('./Datasets/active_vulnerability_index') as f:
-        data = json.load(f)
-    return data
+    
+    return active_vulnerability_index
 
 @app.route("/health_workers")
 def health_workers():
-    with open('./Datasets/healthworkers_index') as h:
-        health_workers = json.load(h)
+    
     return health_workers
 
 @app.route("/school")
 def school():
-    with open('./Datasets/school_index') as s:
-        school = json.load(s)
+    
     return school
 
 if __name__=='__main__':
