8677b77e18bcd63fa6b69d2c90c8bbeec459e87f
vaishnav-197 <vaishnavea18@gmail.com>
2021-01-05 01:20:36 +0530

    major

--- test.py
+++ test.py
@@ -3,9 +3,13 @@ import pandas as pd
 import json
 
 state_0=[]
-with open('./Datasets/New Folder/phase2_state_50_60') as s:
-        phase1_india_json = json.load(s)
-        phase1_india_json=json.dumps(phase1_india_json,indent=4)
-print(phase1_india_json)
+with open('./Datasets/New Folder/phase3_state_15_19') as s:
+        test = json.load(s)
+        test=json.dumps(test,indent=4)
+# for i in test:
+#         print(i)
+        
+
+        print(test)
 
 
