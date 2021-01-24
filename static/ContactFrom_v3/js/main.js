03a789a031790123bf3b5fe7d00bd9fc9f072fb2
neeraj2403 <jayarajneeraj@gmail.com>
2021-01-24 16:30:02 +0530

    validated

--- static/ContactFrom_v3/js/main.js
+++ static/ContactFrom_v3/js/main.js
@@ -35,63 +35,112 @@
     
     /*==================================================================
     [ Validate ]*/
-    var name = $('.validate-input input[name="name"]');
-    var Age = $('.validate-input input[name="Age"]');
-    var occupation = $('.validate-input textarea[name="occupation"]');
-    var state = $('.validate-input textarea[name="state"]');
+    // var name = $('.validate-input input[name="name"]');
+    // var Age = $('.validate-input input[name="Age"]');
+    // var occupation = $('.validate-input textarea[name="occupation"]');
+    // var state = $('.validate-input textarea[name="state"]');
 
     
 
-    $('.validate-form').on('submit',function(){
-        var check = true;
-        // $('.modal').modal('show',function(e){
+    // $('.validate-form').on('submit',function(){
+    //     var check = true;
+    //     // $('.modal').modal('show',function(e){
 
-        //  e.preventDefault();
+    //     //  e.preventDefault();
 
-        // });
-        //  e.preventDefault();
-        if($(name).val().trim() == ''){
-            showValidate(name);
-            check=false;
-        }
-
-
-        if($(Age).val().trim() == '') {
-            showValidate(Age);
-            check=false;
-        }
-
-        if($(occupation).val().trim() == ''){
-            showValidate(occupation);
-            check=false;
-        }
-        if($(state).val().trim() == ''){
-            showValidate(state);
-            check=false;
-        }
-
-        return check;
-    });
+    //     // });
+    //     //  e.preventDefault();
+    //     if($(name).val().trim() == ''){
+    //         showValidate(name);
+    //         check=false;
+    //     }
 
 
-    $('.validate-form .input3').each(function(){
-        $(this).focus(function(){
-           hideValidate(this);
-       });
-    });
+    //     if($(Age).val().trim() == '') {
+    //         showValidate(Age);
+    //         check=false;
+    //     }
 
-    function showValidate(input) {
-        var thisAlert = $(input).parent();
+    //     if($(occupation).val().trim() == ''){
+    //         showValidate(occupation);
+    //         check=false;
+    //     }
+    //     if($(state).val().trim() == ''){
+    //         showValidate(state);
+    //         check=false;
+    //     }
 
-        $(thisAlert).addClass('alert-validate');
-    }
+    //     return check;
+    // });
 
-    function hideValidate(input) {
-        var thisAlert = $(input).parent();
 
-        $(thisAlert).removeClass('alert-validate');
-    }
+    // $('.validate-form .input3').each(function(){
+    //     $(this).focus(function(){
+    //        hideValidate(this);
+    //    });
+    // });
+
+    // function showValidate(input) {
+    //     var thisAlert = $(input).parent();
+
+    //     $(thisAlert).addClass('alert-validate');
+    // }
+
+    // function hideValidate(input) {
+    //     var thisAlert = $(input).parent();
+
+    //     $(thisAlert).removeClass('alert-validate');
+    // }
     
     
+    
+
 
 })(jQuery);
+
+
+function validateForm() {
+    // var x = document.forms["myform"]["name"];
+    // console.log(x)
+    // if (x.value == "") {
+    //   window.alert("Pincode must be filled");
+    //   console.log("Pincode must be filled")
+    //   return false;
+    // }
+    var y = document.forms["myform"]["lat"];
+    var x = document.forms["myform"]["lon"];
+    var z = document.forms["myform"]["name"];
+    var a = document.forms["myform"]["Age"];
+    var b = document.forms["myform"]["occupation"];
+    var c = document.forms["myform"]["State"];
+    
+
+
+
+
+
+    if (y.value == "") {
+      window.alert("All Fields must be filled");
+      return false;
+    }
+    if (x.value == "") {
+        window.alert("All Fields must be filled");
+        return false;
+      }
+    if (z.value == "") {
+        window.alert("All Fields must be filled");
+        return false;
+      }
+      if (a.value == "") {
+        window.alert("All Fields must be filled");
+        return false;
+      }
+      if (b.value == "Occupation") {
+        window.alert("All Fields must be filled");
+        return false;
+      }
+      if (c.value == "Select State") {
+        window.alert("All Fields must be filled");
+        return false;
+      }
+  }
